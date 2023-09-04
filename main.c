b86703d74687ff27effbe5f6a580decb6340ae5f
devv2632 <devmehta2632@gmail.com>
2023-09-05 02:51:37 +0530

    add file

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 void welcome();
+int account_data();
 void main()
  {
     welcome();
+    account_data();
 
  }
  void welcome()
  {
     printf("--WELCOME TO MITHILA BANK--");
-    printf("hi");
+ }
+ int account_data()
+ {
+    FILE *file = fopen("accountdata.txt","a");
+    fprintf(file,"hey this is dev");
+    fclose(file);
  }
